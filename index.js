329ea2f014733c7dc596b12530e45e201a414c00
David Sotomayor <david.elijah.sotomayor@gmail.com>
2021-01-12 15:38:22 -0600

    edit readme format; fixed undefined issues from mockup

--- index.js
+++ index.js
@@ -4,49 +4,49 @@ const fs = require('fs');
 inquirer
   .prompt([
     {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?'
     },
     {
-        type: 'input',
-        name: 'description',
-        message: 'Please add a description of your project:'
+      type: 'input',
+      name: 'description',
+      message: 'Please add a description of your project:'
     },
     {
-        type: 'input',
-        name: 'installation',
-        message: 'Please enter instructions for installing your project:'
+      type: 'input',
+      name: 'installation',
+      message: 'Please enter instructions for installing your project:'
     },
     {
-        type: 'input',
-        name: 'usage',
-        message: 'Please explain the usage process of your application:'
+      type: 'input',
+      name: 'usage',
+      message: 'Please explain the usage process of your application:'
     },
     {
-        type: 'input',
-        name: 'contribution',
-        message: 'Please enter any contributions that may have helped in your project:'
+      type: 'input',
+      name: 'contribution',
+      message: 'Please enter any contributions that may have helped in your project:'
     },
     {
-        type: 'input',
-        name: 'test_instructions',
-        message: 'Please enter any instructions for testing out your project:'
+      type: 'input',
+      name: 'test_instructions',
+      message: 'Please enter any instructions for testing out your project:'
     },
     {
-        type: 'input',
-        name: 'license',
-        message: 'Please enter the license that your project is protected under:'
+      type: 'input',
+      name: 'license',
+      message: 'Please enter the license that your project is protected under:'
     },
     {
-        type: 'input',
-        name: 'github_username',
-        message: 'What is your github username so that others may reach you?'
+      type: 'input',
+      name: 'github_username',
+      message: 'What is your github username so that others may reach you?'
     },
     {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email so that others may reach you?'
+      type: 'input',
+      name: 'email',
+      message: 'What is your email so that others may reach you?'
     },
     {
       type: 'input',
@@ -58,41 +58,47 @@ inquirer
     console.log(answers);
 
     const readme = `# ${answers.title}
+
+  ## Description
+  ${answers.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributions](#contributions)
+  * [Test Instructions](#test-instructions)
+  * [License](#license)
+  * [Questions](#questions)
+
+  ### Installation 
+  ${answers.installation}
+
+  ### Usage
+  ${answers.usage}
+
+  ### Contributions
+  ${answers.contribution}
+
+  ### Test Instructions
+  ${answers.test_instructions}
+
+  ### License
+  ${answers.license}
     
-    ## Table of Contents
-    
-    ### Description
-    ${answers.description}
-    
-    ### Installation 
-    ${answers.installation}
+  ### Questions
+  ${answers.github_username}
+  ${answers.email}
 
-    ### Usage
-    ${answers.usage}
+  Copyright &copy; ${answers.first_last_name}`
 
-    ### Contributions
-    ${answers.contributions}
-
-    ### Test Instructions
-    ${answers.test_instructions}
-
-    #### License
-    ${answers.license}
-    
-    #### Questions
-    ${answers.github_username}
-    ${answers.email}
-
-    Copyright &copy; ${answers.first_last_name}`
-
-    fs.writeFile('message.txt', readme, (err) => {
+    fs.writeFile('message.md', readme, (err) => {
       if (err) throw err;
       console.log('The file has been saved!');
-});
+    });
 
   })
   .catch(error => {
-    if(error.isTtyError) {
+    if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
     } else {
       // Something else when wrong
